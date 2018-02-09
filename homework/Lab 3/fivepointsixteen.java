8bea157a99630d06803a6332a792f84ec7d8cfd5
namhoang1 <nam.hoang1@marist.edu>
2018-02-09 15:46:48 -0500

    da

---
+++ homework/Lab 3/fivepointsixteen.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class fivepointsixteen {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a positive number:");
+		int number = input.nextInt();
+		int factors;
+		if (isPrime (number)) {
+			System.out.print("The prime factors are: " + number + " ");
+			for (int i = 1; i < number; i++) {
+				factors = number % i;
+				if (factors == 0 && isPrime(i))
+					System.out.print(i + " ");
+				}
+		} else {
+			System.out.print("The prime factors are: ");
+			for (int i = 1; i < number; i++) {
+				factors = number % i;
+				if (factors == 0 && isPrime(i))
+					System.out.print(i + " ");
+			}
+		}
+	}	
+		
+	
+	public static boolean isPrime (int n) {
+		boolean prime = true;
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
+				prime = false;
+				break;
+			}
+
+		}
+		return prime;
+	}
+
+}
+
+
+  
